2c9d8c7da6e98d324bb1de6347ae922af99497ae
Jose Delgado <j3leonardodelgado@gmail.com>
2023-07-09 21:44:34 -0400

    fixed (no duplicate item in fav list)

--- rick_morty_challenge/src/App.jsx
+++ rick_morty_challenge/src/App.jsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [allCharacter, setAllCharacter] = useState([
-    { name: "leonardo", image: "" ,species:""},
-  ]);
+  const [allCharacter, setAllCharacter] = useState([]);
 
   const [listFav, setListFav] = useState(() =>
     JSON.parse(localStorage.getItem("rickAndMorty"))
   );
 
   function addFav(character) {
+    const isInFavList = (personaje) =>{ return personaje.id === character.id }
+    if (listFav.some(isInFavList)){
+      return
+    }
+
     setListFav((oldListFav) => {
       localStorage.setItem(
         "rickAndMorty",
@@ -32,26 +35,43 @@ function App() {
     });
   }
 
- const filterSearch = (specie)=>{
-  const getCharacterList = async () => {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/?species=${specie}`);
-    const json = await response.json();
-    const cleanJson = json.results.map((element) => {
-      return {id:element.id, name: element.name, image: element.image, species:element.species,status:element.status };
-    });
-    setAllCharacter(cleanJson);
-  };
-  getCharacterList();
+  const filterSearch = (specie) => {
+    const getCharacterList = async () => {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/?species=${specie}`
+      );
+      const json = await response.json();
+      const cleanJson = json.results.map((element) => {
+        return {
+          id: element.id,
+          name: element.name,
+          image: element.image,
+          species: element.species,
+          status: element.status,
+        };
+      });
 
- }
+      setAllCharacter(cleanJson);
+    };
+    getCharacterList();
+  };
 
   useEffect(() => {
     const getCharacterList = async () => {
-      const response = await fetch("https://rickandmortyapi.com/api/character/");
+      const response = await fetch(
+        "https://rickandmortyapi.com/api/character/"
+      );
       const json = await response.json();
       const cleanJson = json.results.map((element) => {
-        return { id:element.id,name: element.name, image: element.image, species:element.species,status:element.status };
+        return {
+          id: element.id,
+          name: element.name,
+          image: element.image,
+          species: element.species,
+          status: element.status,
+        };
       });
+      console.log(cleanJson);
       setAllCharacter(cleanJson);
     };
     getCharacterList();
@@ -84,16 +104,17 @@ function App() {
 
       <h2>All character</h2>
       <label htmlFor="species">filter by: </label>
-      <select name="species"  onChange={e =>{
-        filterSearch(e.target.value)
-      }
-      } >
-      <option value=''>All</option>
-      <option value='human'>Human</option>
-      <option value='alien'>Alien</option>
-      <option value='unknown'>Unknown</option>
+      <select
+        name="species"
+        onChange={(e) => {
+          filterSearch(e.target.value);
+        }}
+      >
+        <option value="">All</option>
+        <option value="human">Human</option>
+        <option value="alien">Alien</option>
+        <option value="unknown">Unknown</option>
       </select>
-   
 
       {allCharacter.map((character) => {
         return (
@@ -107,9 +128,6 @@ function App() {
         );
       })}
     </div>
-
-
-
   );
 }
 
