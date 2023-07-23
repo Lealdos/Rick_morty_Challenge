75e1d4d0bfd9735cfe958681778ac956af6e69bf
Jose Delgado <j3leonardodelgado@gmail.com>
2023-07-23 11:31:28 -0400

    add feacture (filter by specie)

--- rick_morty_challenge/src/App.jsx
+++ rick_morty_challenge/src/App.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
   let url = new URL('https://rickandmortyapi.com/api/character/');
+
+  // usar un hook para la url e inicializarlo como inicialice el listFav con una funcion anonima que retorne su valor inicial y luego cambiar eso en el codigo
   const [allCharacter, setAllCharacter] = useState([]);
 
   const [listFav, setListFav] = useState(() => {
@@ -11,14 +13,15 @@ function App() {
   });
 
   function addFav(character) {
-    const isInFavList = (personaje) =>{ return personaje.id === character.id }
-    if (listFav.some(isInFavList)){
-      return
+    const isInFavList = (personaje) => {
+      return personaje.id === character.id;
+    };
+    if (listFav?.some(isInFavList)) {
+      return;
     }
-
     setListFav((oldListFav) => {
       localStorage.setItem(
-        "rickAndMorty",
+        'rickAndMorty',
         JSON.stringify([...oldListFav, character])
       );
       return [...oldListFav, character];
@@ -28,13 +31,18 @@ function App() {
   function deleteFav(character) {
     setListFav((oldListFav) => {
       localStorage.setItem(
-        "rickAndMorty",
+        'rickAndMorty',
         JSON.stringify(
           oldListFav.filter((element) => element.name != character.name)
         )
       );
       return oldListFav.filter((element) => element.name != character.name);
     });
+  }
+
+  const pagination = (data) => {
+
+
   }
 
 
@@ -53,40 +61,54 @@ function App() {
     return cleanJson
   }
 
-
   const getCharacterList =  async (api) => {
-    setAllCharacter(await fechingApi(api));
- };
+     setAllCharacter(await fechingApi(api));
+  };
 
+  const filterSearch = (specie) => {
+    let params = new URLSearchParams(url.search);
+    params.set('species', specie);
+    if (specie === 'all') {
+      params.delete('species');
+      return getCharacterList(url.href)
+    }
+    url.search = params.toString();
+    let updatedUrl = url.toString();
+    return getCharacterList(updatedUrl);
+  };
 
-
- useEffect(() => {
-  getCharacterList(url.href);
-}, []);
-
+  useEffect(() => {
+    getCharacterList(url.href);
+  }, []);
+  
   return (
     <div>
       <h1>Rick and Morty character</h1>
-
+        <button onClick= {()=>pagination(url.href)}>next</button> 
+        <button>prev</button> 
       <form>
         <input type="text" placeholder="serching for character" disabled />
         <button>search</button>
       </form>
 
       <h2>favorites character</h2>
-      {listFav.map((character) => {
+      <div className='cards'>
+      {listFav?.map((character) => {
         return (
+          
           <div className="card" key={character.id}>
-            <h3>{character.name}</h3>
             <h5>Specie: {character.species}</h5>
             <h5>Status: {character.status}</h5>
-            <img src={character.image} alt={character.name + " image"} />
+            <img src={character.image} alt={character.name + ' image'} />
+            <h3>{character.name}</h3>
             <button onClick={() => deleteFav(character)}>
               Delete from fav
             </button>
           </div>
         );
       })}
+
+      </div>
 
       <hr />
 
@@ -98,23 +120,26 @@ function App() {
           filterSearch(e.target.value);
         }}
       >
-        <option value="">All</option>
+        <option value="all">All</option>
         <option value="human">Human</option>
         <option value="alien">Alien</option>
         <option value="unknown">Unknown</option>
       </select>
 
+      
+      <div className="cards">
       {allCharacter.map((character) => {
         return (
           <div className="card" key={character.id}>
-            <h3>{character.name}</h3>
             <h5>Specie: {character.species}</h5>
             <h5>Status: {character.status}</h5>
-            <img src={character.image} alt={character.name + " image"} />
+            <img src={character.image} alt={character.name + ' image'} />
+            <h3>{character.name}</h3>
             <button onClick={() => addFav(character)}>Fav</button>
           </div>
         );
       })}
+      </div>
     </div>
   );
 }
